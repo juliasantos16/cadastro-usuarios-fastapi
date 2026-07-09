e5fccc717249954d83c5cd020817f3f4d713bfe4
Júlia Maria <mjulia.santos04@gmail.com>
2026-07-09 17:56:01 -0300

    config: setting up connection to db

--- src/models/settings/database_connection_handler_test.py
+++ src/models/settings/database_connection_handler_test.py
@@ -1,7 +1,8 @@
 import pytest
 from .database_connection_handler import DBConnectionHandler
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio # Teste de integração
+@pytest.mark.skip(reason="connecting with db")
 async def test_connection():
     async with DBConnectionHandler() as db_handler:
         print()
