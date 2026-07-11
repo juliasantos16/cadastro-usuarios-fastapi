3e1557ae297d9cea4cae93d7676e8babd000d095
Júlia Maria <mjulia.santos04@gmail.com>
2026-07-11 16:27:31 -0300

    feat: add POST /users route and UserInput validador

--- src/main/routes/users_routes.py
+++ src/main/routes/users_routes.py
@@ -1,19 +1,32 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from src.main.composer.user_finder_composer import user_finder_composer
 from src.views.http_types.http_request import HttpRequest
+
+from src.main.composer.user_finder_composer import user_finder_composer
+from src.main.composer.user_register_composer import user_register_composer
+
+from src.validators.user_register_validator import UserInput
+
+
+
 
 users_routes = APIRouter(tags=['Usuários'])
 
 
 @users_routes.post('/users')
-async def criar_usuario():
+async def criar_usuario(body: UserInput):
+    http_request = HttpRequest(body=body.model_dump())
+    user_register = user_register_composer()
 
+    http_response = await user_register.handle_register_user(http_request)
     return JSONResponse(
-        content={'olá': 'mundo'},
-        status_code=200
+        content=http_response.body,
+        status_code=http_response.status_code
     )
 
+
+
+# rota de busca
 @users_routes.get('/users/{user_name}')
 async def find_users_by_name(user_name: str):
     http_request = HttpRequest (path_params={'user_name': user_name})
